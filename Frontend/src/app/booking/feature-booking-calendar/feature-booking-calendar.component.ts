dea937bb850341cf113403937cccc22fd0343eae
charlie <sxergiu@gmail.com>
2025-05-29 09:59:35 +0300

    routing and nav calendar

---
+++ Frontend/src/app/booking/feature-booking-calendar/feature-booking-calendar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-booking-calendar',
+  imports: [],
+  templateUrl: './feature-booking-calendar.component.html',
+  styleUrl: './feature-booking-calendar.component.scss'
+})
+export class FeatureBookingCalendarComponent {
+
+}
