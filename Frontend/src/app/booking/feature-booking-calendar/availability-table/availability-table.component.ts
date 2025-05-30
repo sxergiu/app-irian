490c4d183ba47b9a865973cc6951ba633124906b
charlie <sxergiu@gmail.com>
2025-05-30 11:59:03 +0300

    refactor availability table + style

--- Frontend/src/app/booking/feature-booking-calendar/availability-table/availability-table.component.ts
+++ Frontend/src/app/booking/feature-booking-calendar/availability-table/availability-table.component.ts
@@ -1,9 +1,9 @@
-import {Component, Input, Output, EventEmitter, input} from '@angular/core';
+import {Component, effect, input, output} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatTableModule} from '@angular/material/table';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import {AvailableRoomModel} from '../../domain/available.room.model';
+import {AvailableRoomModel, Timeslot} from '../../domain/available.room.model';
 
 @Component({
   selector: 'app-availability-table',
@@ -17,10 +17,37 @@ import {AvailableRoomModel} from '../../domain/available.room.model';
 export class AvailabilityTableComponent {
 
   rooms = input<AvailableRoomModel[]>([])
-  @Input() timeSlots: string[] = [];
-  @Output() slotSelected = new EventEmitter<{ roomId: number; slot: string }>();
+  slotSelected = output<AvailableRoomModel>();
+  dataSource = new MatTableDataSource<AvailableRoomModel>(this.rooms());
+
+  constructor() {
+
+    effect(() => {
+      this.dataSource.data = this.rooms();
+    });
+  }
 
   get displayedColumns(): string[] {
-    return ['room', ...this.timeSlots];
+    return ['room','intervals'];
+  }
+
+  selectInterval(interval: any) {
+    console.log('Selected interval: ' + interval);
+  }
+
+  getIntervalOffset(interval: Timeslot): number {
+    const totalMinutes = 14 * 60; // from 07:00 to 21:00
+    return ((interval.startTime - 7 * 60) / totalMinutes) * 100;
+  }
+
+  getIntervalWidth(interval: Timeslot): number {
+    const totalMinutes = 14 * 60;
+    return ((interval.endTime - interval.startTime) / totalMinutes) * 100;
+  }
+
+  formatInterval(interval: Timeslot): string {
+    const format = (min: number) =>
+      `${Math.floor(min / 60).toString().padStart(2, '0')}:${(min % 60).toString().padStart(2, '0')}`;
+    return `${format(interval.startTime)} - ${format(interval.endTime)}`;
   }
 }
--- Frontend/src/app/booking/feature-booking-calendar/feature-booking-calendar.component.ts
+++ Frontend/src/app/booking/feature-booking-calendar/feature-booking-calendar.component.ts
@@ -2,10 +2,9 @@ import {Component, inject, signal} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RoomFilterComponent} from './room-filter/room-filter.component';
 import {AvailabilityTableComponent} from './availability-table/availability-table.component';
-import {timeSlots} from '../data/timeslots.data';
 import {RoomFilterModel} from '../domain/room.filter.model';
 import {featureBookingCalendarStore} from './+store/feature-booking-calendar.store';
-import {Timeslot} from '../domain/available.room.model';
+import {AvailableRoomModel, Timeslot} from '../domain/available.room.model';
 
 @Component({
   selector: 'app-feature-booking-calendar-page',
@@ -18,36 +17,15 @@ import {Timeslot} from '../domain/available.room.model';
 
 export class FeatureBookingCalendarComponent {
 
-  protected readonly timeSlots = timeSlots;
-
   store = inject(featureBookingCalendarStore);
 
   onFilterChange(filter: RoomFilterModel) {
     this.store.updateFilters(filter);
   }
 
-  onSlotSelected(data: { roomId: number, slot: string }) {
+  onSlotSelected(data: AvailableRoomModel) {
     console.log('User clicked:', data);
   }
 
-  selectInterval(interval: any) {
-    console.log('Selected interval: ' + interval);
-  }
-
-  getIntervalOffset(interval: Timeslot): number {
-    const totalMinutes = 14 * 60; // from 07:00 to 21:00
-    return ((interval.startTime - 7 * 60) / totalMinutes) * 100;
-  }
-
-  getIntervalWidth(interval: Timeslot): number {
-    const totalMinutes = 14 * 60;
-    return ((interval.endTime - interval.startTime) / totalMinutes) * 100;
-  }
-
-  formatInterval(interval: Timeslot): string {
-    const format = (min: number) =>
-      `${Math.floor(min / 60).toString().padStart(2, '0')}:${(min % 60).toString().padStart(2, '0')}`;
-    return `${format(interval.startTime)} - ${format(interval.endTime)}`;
-  }
 
 }
