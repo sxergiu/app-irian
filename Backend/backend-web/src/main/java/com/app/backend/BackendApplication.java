ff80c3166dfdac9cb77683091cc17e640f365ed8
charlie <sxergiu@gmail.com>
2025-06-01 16:15:17 +0300

    docker files and docker compose

--- Backend/backend-web/src/main/java/com/app/backend/BackendApplication.java
+++ Backend/backend-web/src/main/java/com/app/backend/BackendApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.app.backend")
 @EntityScan(basePackages = "com.app.backend")
 public class BackendApplication {
 
